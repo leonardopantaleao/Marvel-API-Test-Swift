1fb7098961aa7b67a6f4cf9a3e54077d900c6489
Leonardo Panta Leão <leonardopspl@gmail.com>
2023-05-02 11:01:05 -0300

    Initial Commit

---
+++ MarvelAPI/MarvelAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAPIApp.swift
+//  MarvelAPI
+//
+//  Created by LEONARDO P S P LEAO on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
